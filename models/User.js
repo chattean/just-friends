c367ea57dbdadf8bd6090633899943d0fb878c5c
Anuj Chatterjee <anujchatterjee@gmail.com>
2021-03-07 07:46:01 -0800

    Adjusting User thoughts to point to the right schema

--- models/User.js
+++ models/User.js
@@ -3,15 +3,22 @@ const { Schema, model } = require('mongoose')
 
 const UserSchema = new Schema({
     username:{
-        type: String
+        type: String,
+        unique: true,
+        trim: true, 
+        required: true,
     },
     email:{
         // validation for mongoose
-        type: String
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address'],
     }, 
     thoughts:{
         // reference the thought model
         // _id:
+        type: 
     },
     // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
     friends:{
