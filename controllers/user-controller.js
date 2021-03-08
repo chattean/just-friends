847c4dd4021687f6eb9812ecd49457aa311a3e16
Anuj Chatterjee <anujchatterjee@gmail.com>
2021-03-07 17:37:24 -0800

    Added delete User and the bonus delete thoughts

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,13 +1,10 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
+const { db } = require('../models/Thought');
 
 const userController = {
     // get all Users
     getAllUser(req, res) {
-        User.find({})
-        .populate({
-            path: 'thoughts',
-            select: '-__v'
-        })
+        User.find()
         .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -18,7 +15,7 @@ const userController = {
     
     // get one User by id
     getUserById({ params }, res) {
-        User.findOne({ _id: params.id })
+        User.findOne({ _id: params.userId })
         .select('-__v')
         .populate('friends')
         .populate('thoughts')
@@ -46,7 +43,7 @@ const userController = {
 
     // update User by id
     updateUser({ params, body }, res) {
-        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        User.findOneAndUpdate({ _id: params.userId }, { $set: body }, { runValidators: true, new: true })
         .then(dbUserData => {
             if (!dbUserData) {
             res.status(404).json({ message: 'No User found with this id!' });
@@ -59,14 +56,16 @@ const userController = {
     
     // delete User
     deleteUser({ params }, res) {
-        User.findOneAndDelete({ _id: params.id })
+        User.findOneAndDelete({ _id: params.userId })
         .then(dbUserData => {
             if (!dbUserData) {
             res.status(404).json({ message: 'No User found with this id!' });
             return;
             }
-            res.json(dbUserData);
+            //Deleting Thoughts
+            return Thought.deleteMany({ _id: {$in:dbUserData.thoughts}});
         })
+        .then(() => {res.json({message: 'Deleted the User and Thoughts associated with the User'})})
         .catch(err => res.status(400).json(err));
     }
 };
